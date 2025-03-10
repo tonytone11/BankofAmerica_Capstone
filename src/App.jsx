9494b519ea5919e014e17ad46a1aff86ae48c1a7
Carmani H. Jackson <charrisjackson422@gmail.com>
2025-03-10 18:46:15 -0400

    contact form complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import Player from './pages/Player';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Footer from './components/Footer';
-import Player from './pages/Player';
+import Training from './pages/Training';
+import ContactForm from './pages/Contact';
 import './App.css';
 
 
@@ -33,7 +34,7 @@ const App = () => {
         <Route path="/training" element={<Training />} />
         <Route path="/player" element={<Player/>} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/contact" element={<ContactForm/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<Error />} />
