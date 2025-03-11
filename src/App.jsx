3570c2609fcebe570513c208f3ad1e1bba8174c7
Charrisjackson <137785408+Charrisjackson@users.noreply.github.com>
2025-03-11 09:14:48 -0400

    Carmani (#7)
    * pushing backend incomplete
    
    * almost finished db
    
    * database code complete
    
    * contact form complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Profile from './pages/Profile';
 import Player from './pages/Player';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Footer from './components/Footer';
+import Training from './pages/Training';
+import ContactForm from './pages/Contact';
 import './App.css';
 
 
@@ -21,21 +26,21 @@ const PlaceholderPage = ({ title }) => {
 
 const App = () => {
   return (
-    <Router>
+<>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<PlaceholderPage title="Profile" />} />
-        <Route path="/training" element={<PlaceholderPage title="Training" />} />
-        <Route path="/players" element={<Player />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/training" element={<Training />} />
+        <Route path="/player" element={<Player/>} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<PlaceholderPage title="Contact" />} />
-        <Route path="/login" element={<PlaceholderPage title="Login" />} />
-        <Route path="/signup" element={<PlaceholderPage title="Sign Up" />} />
-        <Route path="*" element={<PlaceholderPage title="404 - Not Found" />} />
+        <Route path="/contact" element={<ContactForm/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
-    </Router>
+      </>
   );
 };
 
