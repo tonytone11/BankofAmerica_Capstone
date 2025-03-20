e555f7ab350a632be809fbe9efd74d81c97bdabc
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2025-03-20 02:58:38 -0400

    #5.2 debugging new errors from postgreSQL

--- src/backend/config/db.config.js
+++ src/backend/config/db.config.js
@@ -12,6 +12,19 @@ const pool = new Pool({
     }
 });
 
+// Set the search_path to public schema
+pool.query('SET search_path TO public')
+    .then(() => {
+        console.log('Search path set to public schema');
+    })
+    .catch(err => {
+        console.error('Error setting search path:', err);
+    });
+
+pool.on('connect', (client) => {
+    client.query('SET search_path TO public');
+});
+
 // Test connection on startup
 pool.on('connect', () => {
     console.log('Connected to PostgreSQL database');
