aeba49adfff6eee22316b396da4bbb29681dbcb9
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2025-03-20 13:09:21 -0400

    path changes for player page

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // In vite.config.js
   server: {
     proxy: {
       '/football-api': {
-        target: 'https://v3.football.api-sports.io',
+        target: 'https://api-football-v1.p.rapidapi.com/v3',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/football-api/, ''),
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        }
+        rewrite: (path) => path.replace(/^\/football-api/, '')
       }
     }
   },
